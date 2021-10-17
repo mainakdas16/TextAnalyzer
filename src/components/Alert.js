6e7bfb2a0c20f2dba37fac564c60decb3160d181
mainakdas16 <86925839+mainakdas16@users.noreply.github.com>
2021-10-18 02:44:56 +0530

    commit on master

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Alert(props) {
     return (
-        props.alert && <div class="alert alert-success alert-dismissible fade show" role="alert">
+        props.alert && <div className="alert alert-success alert-dismissible fade show" role="alert">
             <strong>{props.alert.type}</strong>:  {props.alert.message}
         </div>
     )
